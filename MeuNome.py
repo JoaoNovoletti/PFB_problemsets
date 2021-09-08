eed4bbf3c3d603470df0e7098b8a848be6502ecd
Joao Novoletti <joao.novoletti@usp.br>
2021-09-08 23:32:29 +0000

    MeuNome.py (atividade Phyton01 problemset) atualizada e resolvida - lembrar de colocar 4 parâmetros ao executar o programa

--- MeuNome.py
+++ MeuNome.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 print("Meu nome: João")
+print("Minha cor favorita: Azul")
 
+atividadefav = "Ir à academia"
+print ("Minha atividade favorita:", atividadefav)
+
+animalfav = "Cachorro"
+print ("Meu animal favorito:", animalfav)
+
+print()
+import sys
+nome = sys.argv[1]
+corfav = sys.argv[2]
+ativfav = sys.argv [3]
+animfav = sys.argv [4]
+
+print ("Meu nome é" , nome + ", minha cor favorita é" , corfav + ", minha atividade favorita é", ativfav + " e meu animal favorito é" , animfav)
