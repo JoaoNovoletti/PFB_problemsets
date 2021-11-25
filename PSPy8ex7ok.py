ec4e1f6588a5dfb790496eb30effee6412148986
Joao Novoletti <joao.novoletti@usp.br>
2021-11-25 20:29:05 +0000

    exercicio 7 finalizado. At~encao ao 91_g1_il

---
+++ PSPy8ex7ok.py
@@ -0,0 +1,171 @@
+#!usr/bin/env python3
+
+#Começa o 4
+import re
+
+seqs={}
+codons={}
+arquivo = input('Arquivo fasta: ')
+
+with open(arquivo, "r") as R_arquivo:
+  for line in R_arquivo:
+    line = line.rstrip()
+
+    if line[0] == '>':
+
+     achandogene = re.search(r">(\w+)\s", line)
+     gene = (achandogene.group(1))
+
+     if gene not in seqs.keys():
+       seqs[gene]=''
+
+    else:
+      seqs[gene] += line
+
+for gene in seqs.keys():
+  if gene not in codons.keys():
+    codons[gene]={}
+for gene in codons:
+  codons[gene]["frame-1"] = re.findall(r"(.{3})",seqs[gene])
+  codons[gene]["frame-2"] = re.findall(r"(.{3})",seqs[gene][1:])
+  codons[gene]["frame-3"] = re.findall(r"(.{3})",seqs[gene][2:])
+  codons[gene]["frame-4"] = re.findall(r"(.{3})",seqs[gene][::-1])
+  codons[gene]["frame-5"] = re.findall(r"(.{3})",seqs[gene][-2::-1])
+  codons[gene]["frame-6"] = re.findall(r"(.{3})",seqs[gene][-3::-1])
+
+with open ("Python_08.codons-6frames.nt", "w") as codonsframe3:
+  for gene in codons:
+    for frame in codons[gene]:
+      frameseq = ""
+      for codon in codons[gene][frame]:
+        frameseq += codon
+        frameseq += ' '
+
+      codonsframe3.write(gene+'-'+frame+'-codons'+'\n'+frameseq+'\n')
+
+#Começa o 5
+
+seqscodons={}
+
+translation_table = {
+    'GCT':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
+    'CGT':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R', 'AGA':'R', 'AGG':'R',
+    'AAT':'N', 'AAC':'N',
+    'GAT':'D', 'GAC':'D',
+    'TGT':'C', 'TGC':'C',
+    'CAA':'Q', 'CAG':'Q',
+    'GAA':'E', 'GAG':'E',
+    'GGT':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G',
+    'CAT':'H', 'CAC':'H',
+    'ATT':'I', 'ATC':'I', 'ATA':'I',
+    'TTA':'L', 'TTG':'L', 'CTT':'L', 'CTC':'L', 'CTA':'L', 'CTG':'L',
+    'AAA':'K', 'AAG':'K',
+    'ATG':'M',
+    'TTT':'F', 'TTC':'F',
+    'CCT':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P',
+    'TCT':'S', 'TCC':'S', 'TCA':'S', 'TCG':'S', 'AGT':'S', 'AGC':'S',
+    'ACT':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T',
+    'TGG':'W',
+    'TAT':'Y', 'TAC':'Y',
+    'GTT':'V', 'GTC':'V', 'GTA':'V', 'GTG':'V',
+    'TAA':'*', 'TGA':'*', 'TAG':'*'
+}
+
+with open("Python_08.codons-6frames.nt", "r") as R_arquivo:
+  for line in R_arquivo:
+    line = line.rstrip()
+    codonfound = re.search(r"[ATCG][ATCG][ATCG]", line)
+    achandogeneframe = re.search(r"(c\d\w+-\w+-\d)", line)
+    if achandogeneframe:
+      geneframe = (achandogeneframe.group(1))
+      if geneframe not in seqscodons.keys():
+       seqscodons[geneframe]=''
+    elif codonfound:
+       seqscodons[geneframe] += line
+
+with open ("Python_08.translated.aa", "w") as traducao:
+  for geneframe in seqscodons:
+    line = seqscodons[geneframe]
+    line = line.rstrip()
+    codonslist = line.split(' ')
+    translatedcodons = []
+    for codon in codonslist:
+      codonfound = re.search(r"[ATCG][ATCG][ATCG]", codon)
+      if codonfound:
+        translatedcodons.append(translation_table[codon])
+    translatedlist = ''
+    for codons in translatedcodons:
+      translatedlist += codons
+    
+    traducao.write(geneframe + "-aa" + '\n' + translatedlist + '\n')
+
+
+#Começa o 6
+
+seqsAA = {}
+
+with open("Python_08.translated.aa", "r") as R_arquivo:
+  for line in R_arquivo:
+    line = line.rstrip()
+    achandogeneAA = re.search(r"(c\d\w+)-(\w+-\d)", line)
+    if achandogeneAA:
+      geneAA = (achandogeneAA.group(1))
+      geneframeAA = (achandogeneAA.group(0))
+      if geneAA not in seqsAA.keys():
+        seqsAA[geneAA]={}
+      if geneframeAA not in seqsAA[geneAA].keys():
+        seqsAA[geneAA][geneframeAA] = ""
+    else:
+       seqsAA[geneAA][geneframeAA] += line
+
+dadospepmaior = {}
+
+with open ("Python_08.translated-longest.aa", "w") as Tlongest:
+  for genaa in seqsAA:
+    maior = 0
+    pepmaior = "pepitide not found in any frame"
+    tamanho = 0
+    sequencia = ""
+    for AAs in seqsAA[genaa]:
+      for pep in re.finditer(r"(M\w*\*)", seqsAA[genaa][AAs]):
+        tamanho = len(pep.group(0))
+        if tamanho > maior:
+          maior = tamanho
+          pepmaior = pep.group(0)
+          sequencia = AAs
+          inicio = pep.start(0)
+          fim = pep.end(0)
+
+    Tlongest.write(genaa + "-longest_peptide" + '\n' + pepmaior + '\n')
+    if sequencia != "":
+      dadospepmaior[sequencia] = []
+      dadospepmaior[sequencia].append(inicio)
+      dadospepmaior[sequencia].append(fim)
+    
+seqscodons = {}
+with open ("Python_08.codons-6frames.nt", "r") as SeqCodonsframe:
+  for line in SeqCodonsframe:
+    line = line.rstrip()
+    codonfound = re.search(r"[ATCG][ATCG][ATCG]", line)
+    achandogeneframe = re.search(r"(c\d\w+-\w+-\d)", line)
+    if achandogeneframe:
+      geneframe = (achandogeneframe.group(1))
+      if geneframe not in seqscodons.keys():
+       seqscodons[geneframe]=''
+    elif codonfound:
+       seqscodons[geneframe] += line
+
+with open("Python_08.orf-longest.nt", "w") as LpepCodons:
+  for seqspep in dadospepmaior:
+   
+    for frame in seqscodons:
+      for pepframe in re.finditer(rf"{seqspep}", frame):
+        correctsequence = seqscodons[frame]
+        seqinicio = dadospepmaior[sequencia][0]*4
+        seqfim = dadospepmaior[sequencia][1]*4
+        subsequence = correctsequence[seqinicio:seqfim]
+
+    fnomedogene = re.search(r"(c\d\w+)", seqspep)
+    nomedogene = fnomedogene.group(0)
+    LpepCodons.write(nomedogene + "-orf-longest_peptide" + '\n' + subsequence + '\n')
+
